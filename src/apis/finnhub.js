d93074c538271e1588d154ac1eca3c48a0aec117
thrila <ifeanyichukwundubuizu@gmail.com>
2023-03-14 21:52:45 +0100

    hide - token key

--- src/apis/finnhub.js
+++ src/apis/finnhub.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const TOKEN = 'cftub39r01qjeql3nlsgcftub39r01qjeql3nlt0'
+
+
 
 export default axios.create({
     baseURL: 'https://finnhub.io/api/v1',
     params:{
-        token: TOKEN,
+        token: import.meta.env.VITE_REACT_APP_API_TOKEN,
         
     }
 })
